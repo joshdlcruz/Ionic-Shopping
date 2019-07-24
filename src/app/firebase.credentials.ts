9a8faeff0901c8b0949a899ac118f10fed783695
joshdlcruz <40876758+joshdlcruz@users.noreply.github.com>
2019-07-24 13:22:02 +0800

    Added AngularFire

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCDsTJYKcvh1rh68jmcZFkZKYQcdvwIMow",
+  authDomain: "angularfire-adbd0.firebaseapp.com",
+  databaseURL: "https://angularfire-adbd0.firebaseio.com",
+  projectId: "angularfire-adbd0",
+  storageBucket: "",
+  messagingSenderId: "625168984779",
+  appId: "1:625168984779:web:32e974a806582e92"
+};
